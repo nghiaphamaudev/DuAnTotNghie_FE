bcc4fde75ff0087e47f3e35566439824dade8966
khanhtd8998 <trankhanhhy199x@gmail.com>
2024-10-31 22:47:37 +0700

    feat: create API category

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './contexts/AuthContext.tsx'
 import { ProductProvider } from './contexts/ProductContext.tsx'
+import { CategoryProvider } from './contexts/CategoryContext.tsx'
 
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
@@ -13,7 +14,9 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <AuthProvider>
         <ProductProvider>
-          <App />
+          <CategoryProvider>
+            <App />
+          </CategoryProvider>
         </ProductProvider>
       </AuthProvider>
     </BrowserRouter>
