7be6f68e5b8ca2f8adce9a02e8b7c4a612a850c9
HuyentnPh32568 <huyentnph32568@fpt.edu.vn>
2024-11-21 20:48:50 +0700

    feat: admin product, category

--- src/contexts/ProductContext.tsx
+++ src/contexts/ProductContext.tsx
@@ -2,12 +2,12 @@ import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAllProduct, getProductById } from "../services/productServices";
 import { useMutation } from "@tanstack/react-query";
-import { Product } from "../common/types/Product";
+import { Products } from "../common/types/Product";
 
 type ProductContextProps = {
-  allProduct: Product[];
-  product: Product | null;
-  setAllProduct: React.Dispatch<React.SetStateAction<Product[]>>;
+  allProduct: Products[];
+  product: Products | null;
+  setAllProduct: React.Dispatch<React.SetStateAction<Products[]>>;
   getAllDataProduct: () => void;
   getDataProductById: (id: string) => void;
 };
@@ -29,15 +29,21 @@ export const ProductProvider = ({
 }) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const nav = useNavigate();
-  const [allProduct, setAllProduct] = useState<Product[]>([]);
-  const [product, setProduct] = useState<Product | null>(null);
+  const [allProduct, setAllProduct] = useState<Products[]>([]);
+  const [product, setProduct] = useState<Products | null>(null);
 
   const { mutateAsync: getAllDataProduct } = useMutation({
     mutationFn: async () => {
-      const data = await getAllProduct();
+      const params = {
+        page: 1,
+        limit: 5,
+        name: "",
+        status: 1,
+      };
+      const data = await getAllProduct(params);
       setAllProduct(data.data);
       return data;
-    }
+    },
   });
 
   const { mutateAsync: getDataProductById } = useMutation({
